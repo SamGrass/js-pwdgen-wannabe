296743cbdbadff448151aeccae59386504bfcfca
Samgrass <samuelegrassetti01@gmail.com>
2024-06-03 15:46:33 +0200

    add items + first prompt

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il suo nome
+let userName = prompt("inserisci il tuo nome");
+
+// chiedi il suo cognome
+
+
+// chiedi il suo colore preferito
+
+
+// somma i risultati per creare una password nomecognomecolorepreferito23
+
+
+// aggiungi ad html
